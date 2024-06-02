5f6236b0fa3e9ffb9e003735e43f05b08c6fc581
s1lver29 <nikitaselver@yandex.ru>
2024-06-02 17:58:05 +0300

    Fix dockerfile, run.sh, build and scoring.py

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 cd server_api
 docker build -t toxic-metr-app .
-docker run --gpus all -it -p 8000:8000 toxic-metr-app
+docker run -it -p 8000:8000 toxic-metr-app
--- server_api/torchapp/toxic_model/scoring.py
+++ server_api/torchapp/toxic_model/scoring.py
@@ -6,28 +6,27 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 from torchapp.toxic_model.predict import get_predictions
 
-log_file = 'torchapp/toxic_model/logs/training.log'
+log_file = "training.log"
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler(log_file),
-        logging.StreamHandler()
-    ]
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler(log_file), logging.StreamHandler()],
 )
 logger = logging.getLogger()
 
+
 def show_confusion_matrix(confusion_matrix) -> None:
     hmap = sns.heatmap(confusion_matrix, annot=True, fmt="d", cmap="Blues")
-    hmap.yaxis.set_ticklabels(hmap.yaxis.get_ticklabels(), rotation = 0, ha='right')
-    hmap.xaxis.set_ticklabels(hmap.xaxis.get_ticklabels(), rotation = 30, ha='right')
-    plt.ylabel('True Sentiment')
-    plt.xlabel('Predicted Sentiment')
+    hmap.yaxis.set_ticklabels(hmap.yaxis.get_ticklabels(), rotation=0, ha="right")
+    hmap.xaxis.set_ticklabels(hmap.xaxis.get_ticklabels(), rotation=30, ha="right")
+    plt.ylabel("True Sentiment")
+    plt.xlabel("Predicted Sentiment")
+
 
 def score_model(model, test_data_loader, class_names, device) -> None:
     y_pred, y_pred_probs, y_test = get_predictions(model, test_data_loader, device)
     logger.info(classification_report(y_test, y_pred, target_names=class_names))
 
     cm = confusion_matrix(y_test, y_pred)
-    df_cm = pd.DataFrame(cm, index=class_names, columns = class_names)
+    df_cm = pd.DataFrame(cm, index=class_names, columns=class_names)
     show_confusion_matrix(df_cm)
